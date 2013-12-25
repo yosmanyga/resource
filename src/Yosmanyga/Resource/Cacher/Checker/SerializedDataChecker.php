5a95c6b0350b9308630ba0deeb89267ce6f80054
Yosmany Garcia <yosmanyga@gmail.com>
2013-12-25 15:21:19 -0500

    Added a "checker" class for resource of type "data"

---
+++ src/Yosmanyga/Resource/Cacher/Checker/SerializedDataChecker.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Yosmanyga\Resource\Cacher\Checker;
+
+use Yosmanyga\Resource\Cacher\Storer\FileStorer;
+use Yosmanyga\Resource\Cacher\Storer\StorerInterface;
+use Yosmanyga\Resource\Resource;
+
+class SerializedDataChecker implements CheckerInterface
+{
+    /**
+     * @var \Yosmanyga\Resource\Cacher\Storer\StorerInterface
+     */
+    private $storer;
+
+    /**
+     * @param \Yosmanyga\Resource\Cacher\Storer\StorerInterface $storer
+     */
+    public function __construct(StorerInterface $storer = null)
+    {
+        $this->storer = $storer ?: new FileStorer();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function supports(Resource $resource)
+    {
+        if (!$resource->hasMetadata('data')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function add(Resource $resource)
+    {
+        $this->storer->add(
+            serialize($resource->getMetadata('data')),
+            $resource
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function check(Resource $resource)
+    {
+        if (!$this->storer->has($resource)) {
+            return false;
+        }
+
+        return $this->storer->get($resource) == serialize($resource->getMetadata('data'));
+    }
+}
---
+++ tests/Yosmanyga/Test/Resource/Cacher/Checker/SerializedDataCheckerTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Yosmanyga\Test\Resource\Cacher\Checker;
+
+use Yosmanyga\Resource\Cacher\Checker\SerializedDataChecker;
+use Yosmanyga\Resource\Cacher\Storer\FileStorer;
+use Yosmanyga\Resource\Resource;
+
+class SerializedDataCheckerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers Yosmanyga\Resource\Cacher\Checker\SerializedDataChecker::__construct
+     */
+    public function testConstructor()
+    {
+        $checker = new SerializedDataChecker();
+        $this->assertAttributeEquals(new FileStorer(), 'storer', $checker);
+
+        /** @var \Yosmanyga\Resource\Cacher\Storer\StorerInterface $storer */
+        $storer = $this->getMock('Yosmanyga\Resource\Cacher\Storer\StorerInterface');
+        $checker = new SerializedDataChecker($storer);
+        $this->assertAttributeEquals($storer, 'storer', $checker);
+    }
+
+    /**
+     * @covers Yosmanyga\Resource\Cacher\Checker\SerializedDataChecker::supports
+     */
+    public function testSupports()
+    {
+        /** @var \Yosmanyga\Resource\Cacher\Storer\StorerInterface $storer */
+        $storer = $this->getMock('Yosmanyga\Resource\Cacher\Storer\StorerInterface');
+        $checker = new SerializedDataChecker($storer);
+
+        // No data metadata
+        $this->assertFalse($checker->supports(new Resource(array())));
+        // Right metadata
+        $this->assertTrue($checker->supports(new Resource(array('data' => 'foo'))));
+    }
+
+    /**
+     * @covers Yosmanyga\Resource\Cacher\Checker\SerializedDataChecker::add
+     */
+    public function testAdd()
+    {
+        $resource = new Resource(array('data' => 'foo'));
+        $storer = $this->getMock('Yosmanyga\Resource\Cacher\Storer\StorerInterface');
+        $storer->expects($this->once())->method('add')->with(serialize($resource->getMetadata('data')), $resource);
+        /** @var \Yosmanyga\Resource\Cacher\Storer\StorerInterface $storer */
+        $checker = new SerializedDataChecker($storer);
+        $checker->add($resource);
+    }
+
+    /**
+     * @covers Yosmanyga\Resource\Cacher\Checker\SerializedDataChecker::check
+     */
+    public function testCheck()
+    {
+        $resource = new Resource(array('data' => 'foo'));
+
+        $storer = $this->getMock('Yosmanyga\Resource\Cacher\Storer\StorerInterface');
+        $storer->expects($this->once())->method('has')->with($resource)->will($this->returnValue(false));
+        /** @var \Yosmanyga\Resource\Cacher\Storer\StorerInterface $storer */
+        $checker = new SerializedDataChecker($storer);
+        $this->assertFalse($checker->check($resource));
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject $storer */
+        $storer = $this->getMock('Yosmanyga\Resource\Cacher\Storer\StorerInterface');
+        $storer->expects($this->once())->method('has')->with($resource)->will($this->returnValue(true));
+        $storer->expects($this->once())->method('get')->with($resource)->will($this->returnValue(serialize($resource->getMetadata('data'))));
+        /** @var \Yosmanyga\Resource\Cacher\Storer\StorerInterface $storer */
+        $checker = new SerializedDataChecker($storer);
+        $this->assertTrue($checker->check($resource));
+
+        /** @var \PHPUnit_Framework_MockObject_MockObject $storer */
+        $storer = $this->getMock('Yosmanyga\Resource\Cacher\Storer\StorerInterface');
+        $storer->expects($this->once())->method('has')->with($resource)->will($this->returnValue(true));
+        $storer->expects($this->once())->method('get')->with($resource)->will($this->returnValue(serialize('bar')));
+        /** @var \Yosmanyga\Resource\Cacher\Storer\StorerInterface $storer */
+        $checker = new SerializedDataChecker($storer);
+        $this->assertFalse($checker->check($resource));
+    }
+}
